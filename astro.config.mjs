f12072ae90094cdc720506f3d6d4950820487801
andrestorresgo <andytorresgonz@gmail.com>
2026-06-02 00:05:16 -0400

    feat: integrated google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,18 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [react()],
+  integrations: [react(), partytown({
+      config: {
+        forward: ['dataLayer', 'gtag'],
+      },
+    },
+  )],
 });
